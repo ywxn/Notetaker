e79c715c67873c526bef1f693bf509aa218570ed
jeanne <harbingerhvh@gmail.com>
2024-03-29 11:35:05 -0700

    fixed host.sh?

--- host.sh
+++ host.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# if on linux run using gunicorn, else use waitress
-if [ "$(uname)" == "Linux" ]; then
-    py -m pip install -r requirements_linux.txt
-    py -m gunicorn -w 4 -b 0.0.0.0:5000 wsgi:app
-else
-    py -m pip install -r requirements_windows.txt
-    py -c "from waitress import serve; from notetaker import app; serve(app, port=5000)"
-fi
+# Assuming you are using Python 3, replace `py` with `python3`
+python3 -m pip install -r requirements.txt
 
-# open the server in the browser
-start http://localhost:5000
+# Use `python3` instead of `py` to run Python scripts
+python3 -c "from waitress import serve; from notetaker import app; serve(app, port=5000)" &
+
+# Wait for the server to start before opening the browser
+sleep 5
+
+# Check for the operating system and open the browser accordingly
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    open http://localhost:5000
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    xdg-open http://localhost:5000
+elif [[ "$OSTYPE" == "msys" ]]; then
+    start http://localhost:5000
+fi
