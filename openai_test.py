1bcbb42d2e470e706a20474e5d556db6126a7620
jeanne <harbingerhvh@gmail.com>
2024-03-28 18:39:29 -0700

    beautified with black

--- openai_test.py
+++ openai_test.py
@@ -2,15 +2,15 @@
 from openai import OpenAI
 
 # read the OPENAI_API_KEY file to get the api_key
-with open('OPENAI_API_KEY', 'r') as file:
+with open("OPENAI_API_KEY", "r") as file:
     api_key = file.read().strip()
 
 
-client = OpenAI(
-api_key=api_key
-)
+client = OpenAI(api_key=api_key)
 
-completion = client.completions.create(model='gpt-3.5-turbo-instruct', prompt="Once upon a time", max_tokens=50)
+completion = client.completions.create(
+    model="gpt-3.5-turbo-instruct", prompt="Once upon a time", max_tokens=50
+)
 print(completion.choices[0].text)
-print(dict(completion).get('usage'))
+print(dict(completion).get("usage"))
 print(completion.model_dump_json(indent=2))
