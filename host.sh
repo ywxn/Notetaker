7b1f215f763a515cabc7a44572a15f3735f0192a
jeanne <harbingerhvh@gmail.com>
2024-03-28 18:38:17 -0700

    fixed windows compatability

--- host.sh
+++ host.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-# Run wsgi.py using gunicorn
-gunicorn -w 4 -b 0.0.0.0:5000 wsgi:app
+# if on linux run using gunicorn, else use waitress
+if [ "$(uname)" == "Linux" ]; then
+    py -m pip install -r requirements_linux.txt
+    py -m gunicorn -w 4 -b 0.0.0.0:5000 wsgi:app
+else
+    py -c "from waitress import serve; from notetaker import app; serve(app, port=5000)"
+fi
+
+# open the server in the browser
+start http://localhost:5000
